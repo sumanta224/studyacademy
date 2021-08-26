f385733b187f096f795fe6ec55a1c1a1af982e54
sumanta <anuamar654@gmail.com>
2021-08-27 00:30:13 +0530

    added

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ var path=require("path");
 var port= process.env.PORT||"40000";
 app.use(express.static(path.join(__dirname,"public")));
 
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 app.get("/ABOUT", (req, res) => {
-    res.sendFile(__dirname + "/#");
+    res.sendFile(__dirname + "/about.html");
 });
-app.get("/CUNTRACT", (req, res) => {
-    res.sendFile(__dirname + "/#");
+app.get("/CONTRACT", (req, res) => {
+    res.sendFile(__dirname + "/contract.html");
 });
 app.get("/NOTES", (req, res) => {
-    res.sendFile(__dirname + "/#");
+    res.sendFile(__dirname + "/notes.html");
+    
 });
+
 app.get("/VIDEOS", (req, res) => {
     res.sendFile(__dirname + "/video.html");
 });
